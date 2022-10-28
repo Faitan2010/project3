29f230f02142ce426729b519bce6ff6e2db1d13c
Faitan2010 <jenia1800@gmail.com>
2022-10-28 18:15:01 +0300

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.scss',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
