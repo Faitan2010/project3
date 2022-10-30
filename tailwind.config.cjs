ccfab9cc52233d26882735b2fe78c3903de1cd8e
Faitan2010 <jenia1800@gmail.com>
2022-10-30 13:55:29 +0200

    finished home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -81,8 +81,9 @@ module.exports = {
                 'country-table-row': '24px 26.88% 32px minmax(0, 1fr)',
                 'country-table': '47% minmax(0, 1fr)',
                 'country-table-flag' : '34.2% minmax(0, 1fr)',
-                'country-table-text': '11.3% minmax(0, 1fr)',
-                'country-table-image': '24px minmax(0, 1fr)'
+                'country-table-text': '32px minmax(0, 1fr)',
+                'country-table-image': '24px minmax(0, 1fr)',
+                'country-table-tablet': '30% minmax(0, 1fr)'
             }
         },
     },
