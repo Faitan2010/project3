4efee922329c04d41dac9e9bd4afa4e1e7a94030
Faitan2010 <jenia1800@gmail.com>
2022-11-01 23:19:10 +0200

    add js on differend maps

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,33 @@ window.addEventListener('load', () => {
         mask.classList.add('mask-none');
     }, 100)
     new WOW().init();
-})
+    const buttonPills = document.querySelectorAll('.buttons')
+    console.log(buttonPills);
+    buttonPills.forEach((item) => {
+        const buttonPill = new ButtonPill(item);
+        // buttonPill.init()
+    });
+});
+
+function ButtonPill(buttonPill) {
+    const _this = this;
+    this.button = [...buttonPill.querySelectorAll('.button')];
+    console.log(this.button);
+    this.blocks = [...buttonPill.querySelectorAll('.pills-block')];
+    this.button.forEach((item, index) => {
+        item.addEventListener('click', function(e) {
+            if(this.classList.contains('active')) {
+                return
+            }
+            const activeIndex = _this.button.findIndex((b) => {
+                return b.classList.contains('active')
+            })
+            _this.button[activeIndex].classList.remove('active')
+            this.classList.add('active')
+            _this.blocks[activeIndex].classList.remove('grid')
+            _this.blocks[activeIndex].classList.add('hidden')
+            _this.blocks[index].classList.remove('hidden')
+            _this.blocks[index].classList.add('grid')
+        })
+    })
+}
